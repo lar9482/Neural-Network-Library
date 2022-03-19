ce19cd61ad296c914272ab3e38b04c17921baae8
Luke Runnels <lar9482@utulsa.edu>
2022-03-19 16:06:32 -0500

    Fixing Input and Output Layers

--- src/Layers/InputLayer.java
+++ src/Layers/InputLayer.java
@@ -11,9 +11,8 @@ public class InputLayer extends Layer{
         initializeLayer(sampleSize, featureSize);
     }
 
-    @Override
-    public void initializeLayer(int sampleSize, int featureSize) throws IllegalArgumentException {
-        this.sampleSize = sampleSize;
+    private void initializeLayer(int layerSize, int featureSize) throws IllegalArgumentException {
+        this.layerSize = layerSize;
         this.featureSize = featureSize;
     }
 
@@ -24,63 +23,63 @@ public class InputLayer extends Layer{
 
     //No implementation for these functions because they will not be used.
     @Override
-    public void feedForward() throws IllegalArgumentException {
+    public void feedForward(String selectedLearningAlgorithm) throws IllegalArgumentException {
         throw new IllegalArgumentException("'feedForward' isn't a legal function in an input layer");
     }
     @Override
-    public void backPropagation(Matrix targetValues) throws IllegalArgumentException{
+    public void backPropagation(Matrix targetMatrix, Matrix resultMatrix) throws IllegalArgumentException{
         throw new IllegalArgumentException("'backPropagation' isn't a legal function in an input layer");
     }
 
     public static void main(String[] args) {
-        ErrorFunction errorFunction = new CrossEntropy();
-        ActivationFunction activation = new sigmoid();
-        int size = 10;
-        double learningRate = 0.5;
-        double[][] data = {
-            {1, 1},
-            {2, 2},
-            {3, 2},
-            {4, 2},
-            {5, 2},
-            {6, 2},
-            {7, 2},
-            {8, 2},
-            {9, 2},
-            {10, 0}
-        };
+        // ErrorFunction errorFunction = new CrossEntropy();
+        // ActivationFunction activation = new sigmoid();
+        // int size = 10;
+        // double learningRate = 0.5;
+        // double[][] data = {
+        //     {1, 1},
+        //     {2, 2},
+        //     {3, 2},
+        //     {4, 2},
+        //     {5, 2},
+        //     {6, 2},
+        //     {7, 2},
+        //     {8, 2},
+        //     {9, 2},
+        //     {10, 0}
+        // };
 
 
-        Matrix dataMatrix = new Matrix(data);
-        InputLayer dummyLayer = new InputLayer(activation, errorFunction, size);
-        DenseLayer testLayer = new DenseLayer(activation, errorFunction, size, dummyLayer);
+        // Matrix dataMatrix = new Matrix(data);
+        // InputLayer dummyLayer = new InputLayer(activation, errorFunction, size);
+        // DenseLayer testLayer = new DenseLayer(activation, errorFunction, size, dummyLayer);
 
 
-        dummyLayer.feedDataIn(dataMatrix);
-        // testLayer.initializeLayer(size);
-        testLayer.setLearningRate(learningRate);
-        9
-        double[][] target = {
-            {1, 1},
-            {2, 2},
-            {3, 2},
-            {4, 2},
-            {5, 2},
-            {6, 2},
-            {7, 2},
-            {8, 2},
-            {9, 2},
-            {10, 0}
-        };
-        Matrix targetMatrix = new Matrix(target);
-        testLayer.feedForward();
-        testLayer.backPropagation(targetMatrix);
+        // dummyLayer.feedDataIn(dataMatrix);
+        // // testLayer.initializeLayer(size);
+        // testLayer.setLearningRate(learningRate);
+        // 9
+        // double[][] target = {
+        //     {1, 1},
+        //     {2, 2},
+        //     {3, 2},
+        //     {4, 2},
+        //     {5, 2},
+        //     {6, 2},
+        //     {7, 2},
+        //     {8, 2},
+        //     {9, 2},
+        //     {10, 0}
+        // };
+        // Matrix targetMatrix = new Matrix(target);
+        // testLayer.feedForward();
+        // testLayer.backPropagation(targetMatrix);
 
-        testLayer.printAttributes();
+        // testLayer.printAttributes();
 
-        testLayer.backPropagation(targetMatrix);
-        testLayer.backPropagation(targetMatrix);
-        testLayer.backPropagation(targetMatrix);
-        testLayer.printAttributes();
+        // testLayer.backPropagation(targetMatrix);
+        // testLayer.backPropagation(targetMatrix);
+        // testLayer.backPropagation(targetMatrix);
+        // testLayer.printAttributes();
     }
 }
